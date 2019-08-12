10dc2c669e4dd50eb8e2366d1e0cfeb754d80738
Mark Strange <strange.mark88@gmail.com>
2019-08-12 14:57:56 -0500

    finish loops ice cream

--- js/while.js
+++ js/while.js
@@ -1,30 +1,14 @@
 "use strict";
 
-// var x = 2;
-//
-// while (x <= 65536) {
-//     console.log(x);
-//     x = (x * 2);
-// }
+var x = 2;
 
+while (x <= 65536) {
+    console.log(x);
+    x = (x * 2);
+}
 
-// An ice cream seller can't go home until she sells all of her cones.
+////////////////////// ICE CREAM /////////////////////////////////////
 
-// Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell.
-
-// Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients.
-
-// Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers
-
-
-// This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-// Math.floor(Math.random() * 5) + 1;
-
-//      5 cones sold...  // if there are enough cones
-//      Cannot sell you 6 cones I only have 3...  // If there are not enough cones
-//      Yay! I sold them all! // If there are no more cones
 
     var allCones = Math.floor(Math.random() * 50) + 50;
 
@@ -43,3 +27,6 @@ do {
     console.log('Yay all cones sold')
 };
 
+
+
+//////////////////////////  ///////////////////////////////////////////
