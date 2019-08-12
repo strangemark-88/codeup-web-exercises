fe24fc2df09f94eff2a5336be99ed26580d49a88
Mark Strange <strange.mark88@gmail.com>
2019-08-12 16:15:51 -0500

    finish pyramid for loop

---
+++ js/for_loops.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// function showMultiplicationTable(num) {
+//     for (var x=1; x<=10; x++) {
+//         console.log(num + ' x ' + x + ' = ' + (num * x));
+//     }
+// }
+// showMultiplicationTable(7);
+
+////////////////////////////////////////////////////////////////////////
+
+
+    //
+    // for (var x=1; x<=10; x++) {
+    // var num = Math.floor(Math.random() * 200) + 20;
+    //     if(num % 2 === 0){
+    //         console.log(num + ' is even');
+    //     } else {
+    //         console.log(num + ' is odd');
+    //     }
+    // }
+
+/////////////////////////////////////////////////////////////////////
+
+// for (var i = 1; i <= 9; i++)
+// {
+//     var num = "";
+//
+//     for (var j = 0; j < i; j++)
+//     {
+//         (num = num + i);
+//     }
+//         console.log(num);
+// }
+
+
+/////////////////////////////////////////////////////////////////////////
+
