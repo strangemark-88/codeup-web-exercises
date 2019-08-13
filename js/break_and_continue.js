8b8c1565faf82f497fc2ade3061f95ce9cfd4ee3
Mark Strange <strange.mark88@gmail.com>
2019-08-13 10:04:16 -0500

    complete loop exercises

---
+++ js/break_and_continue.js
@@ -0,0 +1,34 @@
+"use strict";
+
+
+
+var input, parsedInput = 0;
+
+do {
+    input = prompt("Please enter an odd number between 1-50");
+    parsedInput = parseInt(input);
+
+} while(isNaN(parsedInput) || parsedInput < 0 || parsedInput % 2 === 0);
+
+
+for (var x = 1; x < 50; x+=2) {
+
+    if(parsedInput === x){
+        console.log('Yikes! Skipping number: ' + parsedInput);
+        continue;
+    }
+    else{
+        console.log('Here is an odd Number: ' + x);
+        if(x >= 49){
+            break;
+        }
+    }
+}
+
+// var userInput = +prompt('Enter an odd number between 1-50');
+//
+// userInput.isInteger(parseInt(userInput)) === false)
+//
+// while(userInput % 2 === 0) {
+//     userInput = +prompt('Enter an ODD number between 1-50');
+// }
